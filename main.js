05aa8657ad360a8c93700850cc8f231bde196107
waffles0923 <cloud7717@naver.com>
2021-09-03 20:16:01 +0900

    style : change the icon and add packaging command for windows, linux

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow() {
         width: 800,
         height: 600,
         resizable: false,
+        icon: path.join(__dirname, './assets/icons/png/cursor_icon.png'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
